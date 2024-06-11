7856b691c315933d2e1b0c096446fb75750139cb
Rocco Caliandro <rocco.caliandro@hotmail.com>
2024-06-11 19:12:12 +0200

    modified node2vevc

--- node2vec.py
+++ node2vec.py
@@ -1,19 +1,31 @@
-from sentence_transformers import SentenceTransformer
-import pandas as pd
+#from gensim.models import Word2Vec
 
-# aprire con pandas
-# Antonio TRAIN train = COPIA QUI:
-# Rocco TEST: test = pd.read_csv('/content/drive/MyDrive/BigData Notebooks/dataset/test.csv', sep=',', index_col=0).drop('index',axis=1).drop('label',axis=1)
-test = pd.read_csv('./OneDrive/preprocess/twitter/cleaned/small/test.txt', sep=',', index_col=0).drop('index',axis=1).drop('label',axis=1)
-print(test)
-#train.iloc['text_cleaned']
-model = SentenceTransformer('digio/Twitter4SSE')
-embeddings = model.encode(test['text_cleaned'])
-print(embeddings)
+import scipy
+import gensim
 
-# save into directory
-import pickle
-path = './twitter_dataset/'
-with open(path+'test_embeddings.pkl', "wb") as fOut:
-    pickle.dump({'userId': test['id'], 'embeddings': embeddings}, fOut, protocol=pickle.HIGHEST_PROTOCOL)
+print(scipy.__version__)
 
+size = 128
+
+seed=123
+window=10
+min_count=0
+sg=1
+workers=5 # see if we can increase it
+epochs = 10
+
+
+w2v_model = Word2Vec(vector_size=size, seed=seed, window=window, 
+                     min_count=min_count, sg=sg, workers=workers)
+
+# vocabulary creation
+# token_word = lista di token (lista di liste)
+token_word = [['ciao'], ['poi']]
+w2v_model.build_vocab(token_word, min_count=1)
+total_examples = w2v_model.corpus_count
+
+
+w2v_model.train(token_word, total_examples=total_examples, epochs=epochs)
+
+# w2v_model.wv.key_to_index["ciao"] # torna il dizionario ad ogni parola (k) associa un indice 
+# per prendere l'embedding di ciao, m.model.wv['ciao']
