679fe314f84df9fa881d4b6e15175a0f38ee8433
Rocco Caliandro <rocco.caliandro@hotmail.com>
2024-06-11 20:13:56 +0200

    twitter_clean pipeline

---
+++ twitter_pipeline.py
@@ -0,0 +1,12 @@
+from twitter_clean import launch_twitter_clean
+
+if __name__ == '__main__':
+    # We are going here to launch the first twitter preprocessing
+    launch_twitter_clean('social')
+    launch_twitter_clean('spatial')
+
+    # We are going here to launch the second twitter preprocessing
+    
+    
+    # We are going here to launch the twitter train 
+    
